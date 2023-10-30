49e572e76a54182715c4cd15000c12f8919b0c59
GergelyEva <141821679+GergelyEva@users.noreply.github.com>
2023-10-30 15:42:48 +0300

    Modificare DepozitMagazin

--- proiect3/DepozitMagazin.java
+++ proiect3/DepozitMagazin.java
@@ -1,13 +1,32 @@
 package proiect3;
 
+import proiect3.entitati.inventar.Carte;
+import proiect3.entitati.inventar.IProdus;
+import proiect3.entitati.inventar.Joc;
 import proiect3.gui.Start;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class DepozitMagazin extends JFrame {
+    private static final IProdus demoProd1 =new Joc("Catan", 3, "12346",100.0);
+    private static final IProdus demoProd2 =new Joc("Ticket to Ride", 4, "9274",150.0);
+    private static final IProdus demoProd3 =new Joc("Azul", 7, "2713",84.99);
+    private static final IProdus demoProd4 =new Carte("Prima mea Enciclopedie", 2, "5555",30.0);
+    private static final IProdus demoProd5 =new Carte("Un loc sub soare", 5, "3924",47.5);
+
+    private static final ArrayList<IProdus> produse = new ArrayList<>(
+            Arrays.asList(demoProd1, demoProd2, demoProd3, demoProd4, demoProd5)
+    );
+
+    public ArrayList<IProdus> getProduse(){
+        return produse;
+    }
 
     public DepozitMagazin() {
+
         new Start();
 
 
--- proiect3/DepozitUtils.java
+++ proiect3/DepozitUtils.java
@@ -16,17 +16,9 @@ public class DepozitUtils {
 
     public static final int STRING_LENGTH=15;
 
-    private static final IProdus demoProd1 =new Joc("Catan", 3, "12346",100.0);
-    private static final IProdus demoProd2 =new Joc("Ticket to Ride", 4, "9274",150.0);
-    private static final IProdus demoProd3 =new Joc("Azul", 7, "2713",84.99);
-    private static final IProdus demoProd4 =new Carte("Prima mea Enciclopedie", 2, "5555",30.0);
-    private static final IProdus demoProd5 =new Carte("Un loc sub soare", 5, "3924",47.5);
 
-    public static final ArrayList<IProdus> produse = new ArrayList<>(
-            Arrays.asList(demoProd1, demoProd2, demoProd3, demoProd4, demoProd5)
-    );
 
-    public static void initComIntrate(DefaultListModel<ComandaIntrata> comIntrate){
+        public static void initComIntrate(DefaultListModel<ComandaIntrata> comIntrate){
         Random rand = new Random();
         for(int i=0;i<3;++i){
             ArrayList<ItemComanda> prod = new ArrayList<>();
